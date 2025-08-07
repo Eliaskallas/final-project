10e91e85037a5da54b68fba46f60d44b78a2d3cc
Eliaskallas <eliaskallas@live.com>
2025-08-07 15:09:31 +0300

    Add user_posts.php to get latest 10 posts by user_id

---
+++ backend-blog/api/user_posts.php
@@ -0,0 +1,37 @@
+<?php
+
+include '../connection.php';
+
+header("Content-Type: application/json");
+
+$data = json_decode(file_get_contents("php://input"), true);
+// $data = ["user_id" => 1]; //this line is for testing purposes: i tried other user_ids that are not in the database
+
+// Validation (user_id is required)
+if (!isset($data["user_id"])) {
+    http_response_code(500);
+    echo json_encode([
+        "status" => "error",
+        "message" => "User ID is required"
+    ]);
+    exit;
+}
+
+$userId = (int)$data["user_id"];
+
+// Fetch latest 10 posts for the user
+$result = $mysqli->query("SELECT * FROM posts WHERE user_id = $userId ORDER BY id DESC LIMIT 10"); // look at 3school.com for using order by, DESC and LIMIT
+
+$posts = [];
+$row = $result->fetch_assoc();
+
+while ($row) {
+    $posts[] = $row;
+    $row = $result->fetch_assoc();
+}
+
+echo json_encode([
+    "status" => "success",
+    "message" => "User posts retrieved successfully",
+    "data" => $posts
+]);
